0daa1c809662044e0188787e9c80e01240d3a060
Psarianos Sebastien Angelos <psarian3@teach.cs.toronto.edu>
2026-02-17 23:49:45 -0500

    stuff

--- test.py
+++ test.py
@@ -1,6 +1,38 @@
+
+# fmt: off
+import os  # noqa
+
+if "CUDA_VISIBLE_DEVICES" in os.environ:
+    del os.environ["CUDA_VISIBLE_DEVICES"]
+
+
+from desc import set_device
+
+if True:
+    set_device("gpu", 0)
+
+import jax
+try:
+    print(jax.devices())
+    jax.config.update("jax_compilation_cache_dir", "../jax-caches")
+    jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+    jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+except Exception as e:
+    print(e)
+# fmt: on
+
+
+from desc.continuation import solve_continuation_automatic
 from desc.equilibrium import Equilibrium
 from desc.geometry import FourierRZToroidalSurface
+from desc.optimize import Optimizer
 from desc.plotting import plot_boozer_surface, plot_3d
+from desc.objectives import (
+    ObjectiveFunction, QuasisymmetryTwoTerm, AspectRatio, ForceBalance, FixBoundaryR, FixBoundaryZ, FixPressure, FixCurrent, FixPsi)
+
+from desc.plotting import LinearGrid
+import numpy as np
+
 
 surf = FourierRZToroidalSurface(
     R_lmn=[1, 0.125, 0.1],
@@ -13,8 +45,82 @@ surf = FourierRZToroidalSurface(
 # create initial equilibrium. Psi chosen to give B ~ 1 T. Could also give profiles here,
 # default is zero pressure and zero current
 eq = Equilibrium(M=4, N=4, Psi=0.04, surface=surf)
-plot_boozer_surface(eq)
-plot_3d(eq)
+
+grid = LinearGrid(
+    rho=0.8,
+    theta=np.linspace(0, 2 * np.pi, 100),
+    zeta=np.linspace(0, 2 * np.pi, 100),
+    axis=True
+)
+
+fig = plot_3d(eq, "|F|", log=True, grid=grid)
+fig.write_image("preoptimization.png")
 
 # this is usually all you need to solve a fixed boundary equilibrium
-# eq0 = solve_continuation_automatic(eq, verbose=0)[-1]
+eq0 = solve_continuation_automatic(eq, verbose=0)[-1]
+fig = plot_3d(eq0, "|F|", log=True, grid=grid)
+fig.write_image("postptimization.png")
+
+
+def run_qh_optimization(k, eq):
+    grid = LinearGrid(
+        M=eq.M_grid, N=eq.N_grid, NFP=eq.NFP,
+        rho=np.array([0.6, 0.8, 1.0]),
+        sym=True
+    )
+
+    objective = ObjectiveFunction(
+        QuasisymmetryTwoTerm(eq=eq, helicity=(1, eq.NFP), grid=grid),
+        AspectRatio(eq=eq, target=8, weight=100)
+    )
+
+    # NOTE: Values greater than k are given as constraints
+    R_modes = np.vstack(
+        [0, 0, 0],
+        eq.surface.R_basis.modes[
+            np.max(np.abs(eq.surface.R_basis.modes), 1) > k, :
+        ],
+    )
+    Z_modes = eq.surface.Z_basis.modes[
+        np.max(np.abs(eq.surface.Z_basis.modes), 1) > k, :
+    ]
+
+    constraints = (
+        ForceBalance(eq=eq),
+        FixBoundaryR(eq=eq, modes=R_modes),
+        FixBoundaryZ(eq=eq, modes=Z_modes),
+        FixPressure(eq=eq),
+        FixCurrent(eq=eq),
+        FixPsi(eq=eq)
+    )
+
+    optimizer = Optimizer('proximal-lsq-exact')
+
+    eq_new, history = eq.optimize(
+        objective=objective,
+        constraints=constraints,
+        optimizer=optimizer,
+        maxiter=20,
+        verbose=3,
+        copy=True,
+        options={
+            "initial_trust_ratio": 0.1,
+        }
+    )
+
+    return eq_new
+
+
+grid = LinearGrid(
+    rho=0.8,
+    theta=np.linspace(0, 2 * np.pi, 100),
+    zeta=np.linspace(0, 2 * np.pi, 100),
+    axis=True
+)
+eq_test = eq
+for i in range(0, 3):
+    eq_test = run_qh_optimization(i, eq_test)
+    fig = plot_3d(eq_test, "|F|", log=True, grid=grid)
+    fig.write_image(f"improvement_{i}.png")
+    fig, ax = plot_boozer_surface(eq_test)
+    fig.savefig(f"improvement_{i}_boozer.png")
